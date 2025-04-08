2238843b4ef9955c2b6eee8ee1fc2cea0a772004
Settee <ake14243@gmail.com>
2025-04-09 00:44:28 +0700

    change: IERC20 type to address

--- src/Factory.sol
+++ src/Factory.sol
@@ -7,9 +7,7 @@ import {Pool} from "./Pool.sol";
 import {IERC20Metadata} from "./interfaces/IERC20Metadata.sol";
 
 contract Factory is IFactory {
-    error INVALID_ADDRESS();
-
-    mapping(IERC20 token0 => mapping(IERC20 token1 => address pool)) public override getPool;
+    mapping(address token0 => mapping(address token1 => address pool)) public override getPool;
     mapping(address => PoolInfo) public override getTokens;
 
     uint256 public override totalPools;
@@ -20,17 +18,19 @@ contract Factory is IFactory {
         owner = msg.sender;
     }
 
-    function createPool(IERC20 token0, IERC20 token1) external override returns (address pool) {
+    function createPool(address token0, address token1) external override returns (address pool) {
         if (token0 == token1) {
             revert INVALID_ADDRESS();
         } else if (token0 > token1) {
             (token0, token1) = (token1, token0);
         }
 
-        require(getPool[token0][token1] == address(0), "CREATED");
+        require(getPool[token0][token1] == address(0), CREATED());
 
-        string memory _name = string.concat("Ax11 Liquidity [", token0.name(), "/", token1.name(), "]");
-        string memory _symbol = string.concat("Ax11-LP [", token0.symbol(), "/", token1.symbol(), "]");
+        string memory _name =
+            string.concat("Ax11 Pool [", IERC20Metadata(token0).name(), "/", IERC20Metadata(token1).name(), "]");
+        string memory _symbol =
+            string.concat("Ax11-LP [", IERC20Metadata(token0).symbol(), "/", IERC20Metadata(token1).symbol(), "]");
         pool = address(new Pool{salt: keccak256(abi.encodePacked(token0, token1))}(token0, token1, _name, _symbol));
 
         getPool[token0][token1] = pool;
--- src/interfaces/IFactory.sol
+++ src/interfaces/IFactory.sol
@@ -1,23 +1,69 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import {IERC20} from "./IERC20.sol";
-
 pragma solidity 0.8.28;
 
+/// @title Factory Interface for Ax11 AMM
+/// @notice Interface for the factory contract that deploys and manages Ax11 liquidity pools
 interface IFactory {
-    event PoolCreated(IERC20 indexed token0, IERC20 indexed token1, address pool);
+    /// @notice Error thrown when an invalid address is provided (zero address or identical tokens)
+    error INVALID_ADDRESS();
 
+    /// @notice Error thrown when attempting to create a pool that already exists
+    error CREATED();
+
+    /// @notice Emitted when a new pool is created
+    /// @param token0 The first token of the pool (lower address)
+    /// @param token1 The second token of the pool (higher address)
+    /// @param pool The address of the newly created pool
+    event PoolCreated(address indexed token0, address indexed token1, address pool);
+
+    /// @notice Structure containing information about a pool
+    /// @param token0 The address of the first token in the pool (lower address)
+    /// @param token1 The address of the second token in the pool (higher address)
     struct PoolInfo {
-        IERC20 token0;
-        IERC20 token1;
+        address token0;
+        address token1;
     }
 
-    function getPool(IERC20 token0, IERC20 token1) external view returns (address pool);
+    /// @notice Returns the total number of pools created
+    /// @return The number of pools deployed by this factory
+    function totalPools() external view returns (uint256);
 
-    function getTokens(address pool) external view returns (IERC20 token0, IERC20 token1);
+    /// @notice Returns the address that receives protocol fees
+    /// @return The address where protocol fees are sent
+    function feeTo() external view returns (address);
 
-    function createPool(IERC20 token0, IERC20 token1) external payable returns (address pool);
+    /// @notice Returns the address of the factory owner
+    /// @return The address of the current owner
+    function owner() external view returns (address);
 
+    /// @notice Fetches the pool address for a given pair of tokens
+    /// @dev The order of token0 and token1 doesn't matter, as the factory handles sorting
+    /// @param token0 The address of the first token
+    /// @param token1 The address of the second token
+    /// @return pool The address of the pool for the token pair, or address(0) if it doesn't exist
+    function getPool(address token0, address token1) external view returns (address pool);
+
+    /// @notice Retrieves the tokens associated with a pool
+    /// @param pool The address of the pool
+    /// @return token0 The address of the first token (lower address)
+    /// @return token1 The address of the second token (higher address)
+    function getTokens(address pool) external view returns (address token0, address token1);
+
+    /// @notice Creates a new pool for a pair of tokens
+    /// @dev Tokens are automatically sorted so token0 is always the lower address
+    /// @param token0 The address of the first token
+    /// @param token1 The address of the second token
+    /// @return pool The address of the newly created pool
+    function createPool(address token0, address token1) external returns (address pool);
+
+    /// @notice Updates the owner of the factory
+    /// @dev Can only be called by the current owner
+    /// @param _owner The address of the new owner
     function setOwner(address _owner) external;
+
+    /// @notice Updates the fee recipient address
+    /// @dev Can only be called by the current owner
+    /// @param _feeTo The new address to receive protocol fees
     function setFeeTo(address _feeTo) external;
 }
