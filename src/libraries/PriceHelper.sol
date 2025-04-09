980c6589a3e3e28cc981bf3e7a1106eeb14181cd
Settee <ake14243@gmail.com>
2025-04-09 20:27:40 +0700

    remove: par bin identifier
    -using raw int24 priceId is more efficient

--- src/libraries/PriceHelper.sol
+++ src/libraries/PriceHelper.sol
@@ -7,65 +7,47 @@ import {Uint256x256Math} from "./Math/Uint256x256Math.sol";
 import {SafeCast} from "./Math/SafeCast.sol";
 import {Constants} from "./Constants.sol";
 
+
 /**
- * @title Liquidity Book Price Helper Library
- * @author Trader Joe
- * @notice This library contains functions to calculate prices
+ * @title Price Helper Library
+ * @author Ax11
+ * @notice This library contains functions to calculate prices, modified from Trader Joe's Liquidity Book
  */
 library PriceHelper {
     using Uint128x128Math for uint256;
     using Uint256x256Math for uint256;
-    using SafeCast for uint256;
 
     int256 private constant REAL_ID_SHIFT = 1 << 23;
+    uint256  private constant BINSTEP = 10; // 0.01%
 
     /**
      * @dev Calculates the price from the id and the bin step
      * @param id The id
-     * @param binStep The bin step
      * @return price The price as a 128.128-binary fixed-point number
      */
-    function getPriceFromId(uint24 id, uint16 binStep) internal pure returns (uint256 price) {
-        uint256 base = getBase(binStep);
-        int256 exponent = getExponent(id);
-
-        price = base.pow(exponent);
+    function getPriceFromId(int id) internal pure returns (uint256 price) {
+        uint256 base = getBase();
+        price = base.pow(id);
     }
 
     /**
      * @dev Calculates the id from the price and the bin step
      * @param price The price as a 128.128-binary fixed-point number
-     * @param binStep The bin step
      * @return id The id
      */
-    function getIdFromPrice(uint256 price, uint16 binStep) internal pure returns (uint24 id) {
-        uint256 base = getBase(binStep);
+    function getIdFromPrice(uint256 price) internal pure returns (int id) {
+        uint256 base = getBase();
         int256 realId = price.log2() / base.log2();
-
-        unchecked {
-            id = uint256(REAL_ID_SHIFT + realId).safe24();
-        }
+        id = SafeCast.toInt24(realId);
     }
 
     /**
-     * @dev Calculates the base from the bin step, which is `1 + binStep / BASIS_POINT_MAX`
-     * @param binStep The bin step
+     * @dev Calculates the base from the bin step, which is `1 + BINSTEP / BASIS_POINT_MAX`
      * @return base The base
      */
-    function getBase(uint16 binStep) internal pure returns (uint256) {
+    function getBase() internal pure returns (uint256) {
         unchecked {
-            return Constants.SCALE + (uint256(binStep) << Constants.SCALE_OFFSET) / Constants.BASIS_POINT_MAX;
-        }
-    }
-
-    /**
-     * @dev Calculates the exponent from the id, which is `id - REAL_ID_SHIFT`
-     * @param id The id
-     * @return exponent The exponent
-     */
-    function getExponent(uint24 id) internal pure returns (int256) {
-        unchecked {
-            return int256(uint256(id)) - REAL_ID_SHIFT;
+            return Constants.SCALE + (BINSTEP << Constants.SCALE_OFFSET) / Constants.BASIS_POINT_MAX;
         }
     }
 
