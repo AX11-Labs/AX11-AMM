283565e52444fd435a223aa4ef0a4891af93607f
Settee <ake14243@gmail.com>
2025-04-22 23:59:12 +0700

    add: sync()

--- src/Pool.sol
+++ src/Pool.sol
@@ -239,16 +239,16 @@ contract Pool is Ax11Lp, IPool, ReentrancyGuard, Deadline, NoDelegateCall {
         require(amountX != 0 && amountY != 0, INVALID_AMOUNT()); // at least one of the amount should not be 0
         uint128 feeX = PriceMath.divUp(amountX, 10000); // 0.01% fee
         uint128 feeY = PriceMath.divUp(amountY, 10000); // 0.01% fee
-        uint128 balanceXBefore = IERC20(tokenX).balanceOf(address(this));
-        uint128 balanceYBefore = IERC20(tokenY).balanceOf(address(this));
+        uint128 balanceXBefore = IERC20(tokenX).balanceOf(address(this)).safe128();
+        uint128 balanceYBefore = IERC20(tokenY).balanceOf(address(this)).safe128();
 
         if (amountX != 0) TransferHelper.safeTransfer(tokenX, recipient, amountX);
         if (amountY != 0) TransferHelper.safeTransfer(tokenY, recipient, amountY);
 
         IAx11FlashCallback(callback).flashCallback(feeX, feeY);
 
-        uint128 balanceXAfter = IERC20(tokenX).balanceOf(address(this));
-        uint128 balanceYAfter = IERC20(tokenY).balanceOf(address(this));
+        uint128 balanceXAfter = IERC20(tokenX).balanceOf(address(this)).safe128();
+        uint128 balanceYAfter = IERC20(tokenY).balanceOf(address(this)).safe128();
 
         require(
             balanceXAfter >= balanceXBefore + feeX && balanceYAfter >= balanceYBefore + feeY, INSUFFICIENT_BALANCE()
@@ -403,5 +403,25 @@ contract Pool is Ax11Lp, IPool, ReentrancyGuard, Deadline, NoDelegateCall {
         }
 
         return amountOut;
+    }
+
+    /// @notice Sync the pool balance with the actual balance
+    function sync() external nonReentrant noDelegateCall {
+        uint128 balanceX = IERC20(tokenX).balanceOf(address(this)).safe128();
+        uint128 balanceY = IERC20(tokenY).balanceOf(address(this)).safe128();
+        
+        uint128 currentBalanceX = poolInfo.balanceXLong + poolInfo.balanceXShort;
+        uint128 currentBalanceY = poolInfo.balanceYLong + poolInfo.balanceYShort;
+        
+        if (currentBalanceX != balanceX) {
+            uint128 balanceXLong = PriceMath.fullMulDiv(balanceX, poolInfo.balanceXLong, currentBalanceX).safe128();
+            poolInfo.balanceXLong = balanceXLong;
+            poolInfo.balanceXShort = balanceX - balanceXLong;
+        }
+        if (currentBalanceY != balanceY) {
+            uint128 balanceYLong = PriceMath.fullMulDiv(balanceY, poolInfo.balanceYLong, currentBalanceY).safe128();
+            poolInfo.balanceYLong = balanceYLong;
+            poolInfo.balanceYShort = balanceY - balanceYLong;
+        }
     }
 }
