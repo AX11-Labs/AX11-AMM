b2baab71a86cc5ec8c714ecd13e30a2a3e7c7c98
Settee <ake14243@gmail.com>
2025-04-26 18:30:03 +0700

    update: new fee tiers

--- src/libraries/FeeTier.sol
+++ src/libraries/FeeTier.sol
@@ -6,19 +6,19 @@ library FeeTier {
     function getFee(uint256 priceRange, uint128 value) internal pure returns (uint128 feeAmount) {
         uint128 tier;
         if (priceRange <= 32) {
-            tier = 10;
+            tier = 5; // 0.005%
         } else if (priceRange <= 64) {
-            tier = 50;
+            tier = 10; // 0.01%
         } else if (priceRange <= 128) {
-            tier = 100;
+            tier = 50; // 0.05%
         } else if (priceRange <= 256) {
-            tier = 200;
+            tier = 100; // 0.1%
         } else if (priceRange <= 512) {
-            tier = 300;
+            tier = 200; // 0.2%
         } else if (priceRange <= 1024) {
-            tier = 400;
+            tier = 300; // 0.3%
         } else {
-            tier = 500;
+            tier = 500; // 0.5%
         }
         feeAmount = (value * tier) / 100_000;
     }
