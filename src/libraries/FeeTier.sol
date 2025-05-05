1908dfc1c53e9673e7058b341fb25048ff053548
Settee <ake14243@gmail.com>
2025-05-05 10:03:43 +0700

    update: new price range to support 0.2% bin width

--- src/libraries/FeeTier.sol
+++ src/libraries/FeeTier.sol
@@ -5,20 +5,20 @@ pragma solidity 0.8.28;
 library FeeTier {
     function getFee(uint256 priceRange, uint128 value) internal pure returns (uint128 feeAmount) {
         uint128 tier;
-        if (priceRange < 63) {
-            // 32x32
+        if (priceRange < 31) {
+            // 16x16
             tier = 5; // 0.005%
+        } else if (priceRange < 63) {
+            // 32x32
+            tier = 10; // 0.01%
         } else if (priceRange < 127) {
             // 64x64
-            tier = 10; // 0.01%
+            tier = 50; // 0.05%
         } else if (priceRange < 255) {
             // 128x128
-            tier = 50; // 0.05%
-        } else if (priceRange < 511) {
-            // 256x256
             tier = 100; // 0.1%
         } else {
-            // 512x512
+            // 256x256
             tier = 250; // 0.25%
         }
         feeAmount = (value * tier) / 100_000;
