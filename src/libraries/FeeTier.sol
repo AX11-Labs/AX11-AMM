dc248fde2750078ca3b91383e5a9b9d135086210
Settee <ake14243@gmail.com>
2025-05-09 16:31:00 +0700

    update: use linear interpolation for fee tier calculation

--- src/libraries/FeeTier.sol
+++ src/libraries/FeeTier.sol
@@ -4,23 +4,24 @@ pragma solidity 0.8.28;
 
 library FeeTier {
     function getFee(uint256 priceRange, uint128 value) internal pure returns (uint128 feeAmount) {
-        uint128 tier;
-        if (priceRange < 31) {
-            // 16x16
-            tier = 5; // 0.005%
-        } else if (priceRange < 63) {
-            // 32x32
-            tier = 10; // 0.01%
-        } else if (priceRange < 127) {
-            // 64x64
-            tier = 50; // 0.05%
-        } else if (priceRange < 255) {
-            // 128x128
-            tier = 100; // 0.1%
+        uint8 tier;
+        uint8 minFee = 1; // 0.001%
+        uint8 maxFee = 250; // 0.25%
+        uint256 priceRangeAtMinFee = 15;
+        uint256 priceRangeAtMaxFee = 255;
+
+        if (priceRange <= priceRangeAtMinFee) {
+            tier = minFee;
+        } else if (priceRange >= priceRangeAtMaxFee) {
+            tier = maxFee;
         } else {
-            // 256x256
-            tier = 250; // 0.25%
+            // Linear interpolation:
+            // tier = y0 + (x - x0) * (y1 - y0) / (x1 - x0)
+            // tier = minFee + (priceRange - priceRangeAtMinFee) * (maxFee - minFee) / (priceRangeAtMaxFee - priceRangeAtMinFee)
+            // Perform calculations using uint256 for intermediate products to prevent overflow, then cast to uint16.
+            tier = minFee + uint8((((priceRange - priceRangeAtMinFee) * (maxFee - minFee)) / (priceRangeAtMaxFee - priceRangeAtMinFee))); // won't overflow
         }
-        feeAmount = (value * tier) / 100_000;
+        feeAmount = ((value * tier) / 100_000);
     }
 }
+
