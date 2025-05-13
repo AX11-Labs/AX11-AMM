87d0ff0977d53fa6ba5a3dc68bfa152e81f7da0d
Settee <ake14243@gmail.com>
2025-05-13 17:01:44 +0700

    update: upgrade to minimal multi-token standard

--- src/abstracts/Ax11Lp.sol
+++ src/abstracts/Ax11Lp.sol
@@ -6,7 +6,7 @@ import {IAx11Lp} from '../interfaces/IAx11Lp.sol';
 
 /**
  * @title Ax11Lp
- * @notice Implementation of the IAx11Lp interface for LP token management
+ * @notice Implementation of the AX11 liqudiity token, using minimal multi-token standard.
  * @dev This contract implements a TTL (Time-To-Live) based token approval system instead of the standard ERC20 amount-based allowance.
  *      The allowance mapping stores block timestamps as values, where a timestamp greater than the current block.timestamp
  *      indicates an active approval. This provides a time-limited approval mechanism where approvals automatically expire.
@@ -17,28 +17,19 @@ abstract contract Ax11Lp is IAx11Lp {
                               ERC20 STORAGE
     //////////////////////////////////////////////////////////////*/
 
-    /// @notice Total supply of LP tokens
-    LpInfo internal _totalSupply;
+    /// @notice Total supply of LP tokens by poolId
+    mapping(uint256 => LpInfo) internal _totalSupply;
 
-    /// @notice Mapping of account addresses to their LP token balances
-    mapping(address => LpInfo) public override balanceOf;
+    /// @notice Mapping of account addresses with poolId to their LP token balances
+    mapping(address => mapping(uint256 => LpInfo)) public override balanceOf;
 
     /// @notice Tracks the timestamp-based allowances for token transfers
     /// @dev Uses block.timestamp as the approval metric. A timestamp greater than current block.timestamp indicates an active approval.
     ///      This implements a TTL (Time-To-Live) based approval system where approvals automatically expire.
-    mapping(address => mapping(address => uint256)) public override allowance;
+    mapping(address => mapping(address => mapping(uint256 => uint256))) public override allowance;
 
-    /// @notice Mapping of account addresses to their nonces for permit functionality
-    mapping(address => uint256) public override nonces;
-
-    /// @notice Name of the token
-    string public override name;
-
-    /// @notice Symbol of the token
-    string public override symbol;
-
-    /// @notice Number of decimals used by the token
-    uint8 public constant override decimals = 18;
+    /// @notice Mapping of account addresses with poolId to their nonces for permit functionality
+    mapping(address => mapping(uint256 => uint256)) public override nonces;
 
     /// @notice Initial chain ID for domain separator computation
     uint256 private immutable INITIAL_CHAIN_ID;
@@ -51,11 +42,7 @@ abstract contract Ax11Lp is IAx11Lp {
     //////////////////////////////////////////////////////////////*/
 
     /// @notice Constructs the Ax11Lp contract
-    /// @param _name The name of the token
-    /// @param _symbol The symbol of the token
-    constructor(string memory _name, string memory _symbol) {
-        name = _name;
-        symbol = _symbol;
+    constructor() {
         INITIAL_CHAIN_ID = block.chainid;
         INITIAL_DOMAIN_SEPARATOR = computeDomainSeparator();
     }
@@ -64,40 +51,68 @@ abstract contract Ax11Lp is IAx11Lp {
                               ERC20 LOGIC
     //////////////////////////////////////////////////////////////*/
 
+    /// @notice Returns the name of the token
+    /// @return The name of the token
+    function name() public pure override returns (string memory) {
+        return 'AX11 Liquidity Token';
+    }
+
+    /// @notice Returns the symbol of the token
+    /// @return The symbol of the token
+    function symbol() public pure override returns (string memory) {
+        return 'AX11-LP';
+    }
+
+    /// @notice Returns the number of decimals used by the token
+    /// @return The number of decimals
+    function decimals() public pure override returns (uint8) {
+        return 18;
+    }
+
     /// @notice Returns the total supply of LP tokens
+    /// @param poolId The poolId of the token
     /// @return The total supply information containing long and short positions
-    function totalSupply() public view override returns (LpInfo memory) {
-        return _totalSupply;
+    function totalSupply(uint256 poolId) public view override returns (LpInfo memory) {
+        return _totalSupply[poolId];
     }
 
     /// @notice Approves or revokes permission for a spender to transfer tokens
     /// @param spender The address to approve or revoke permission for
+    /// @param poolId The poolId of the token
     /// @param value block timstamp limit
     /// @return A boolean indicating whether the operation succeeded
-    function approve(address spender, uint256 value) public returns (bool) {
+    function approve(address spender, uint256 poolId, uint256 value) public override returns (bool) {
         address owner = msg.sender;
-        allowance[owner][spender] = value;
-        emit Approval(owner, spender, value);
+        allowance[owner][spender][poolId] = value;
+        emit Approval(owner, spender, poolId, value);
         return true;
     }
 
     /// @notice Transfers LP tokens to another address
     /// @param to The address to transfer tokens to
+    /// @param poolId The poolId of the token
     /// @param longX The amount of long position X tokens to transfer
     /// @param longY The amount of long position Y tokens to transfer
     /// @param shortX The amount of short position X tokens to transfer
     /// @param shortY The amount of short position Y tokens to transfer
     /// @return A boolean indicating whether the transfer succeeded
-    function transfer(address to, uint256 longX, uint256 longY, uint256 shortX, uint256 shortY) public returns (bool) {
+    function transfer(
+        address to,
+        uint256 poolId,
+        uint256 longX,
+        uint256 longY,
+        uint256 shortX,
+        uint256 shortY
+    ) public returns (bool) {
         require(to != address(this), INVALID_ADDRESS());
 
-        LpInfo storage lpInfo_from = balanceOf[msg.sender];
+        LpInfo storage lpInfo_from = balanceOf[msg.sender][poolId];
         lpInfo_from.longX -= longX;
         lpInfo_from.longY -= longY;
         lpInfo_from.shortX -= shortX;
         lpInfo_from.shortY -= shortY;
 
-        LpInfo storage lpInfo_to = balanceOf[to];
+        LpInfo storage lpInfo_to = balanceOf[to][poolId];
         // Cannot overflow because the sum of all user
         // balances can't exceed the max uint256 value.
         unchecked {
@@ -106,7 +121,7 @@ abstract contract Ax11Lp is IAx11Lp {
             lpInfo_to.shortX += shortX;
             lpInfo_to.shortY += shortY;
         }
-        emit Transfer(msg.sender, to, longX, longY, shortX, shortY);
+        emit Transfer(msg.sender, to, poolId, longX, longY, shortX, shortY);
 
         return true;
     }
@@ -114,6 +129,7 @@ abstract contract Ax11Lp is IAx11Lp {
     /// @notice Transfers LP tokens from one address to another
     /// @param from The address to transfer tokens from
     /// @param to The address to transfer tokens to
+    /// @param poolId The poolId of the token
     /// @param longX The amount of long position X tokens to transfer
     /// @param longY The amount of long position Y tokens to transfer
     /// @param shortX The amount of short position X tokens to transfer
@@ -122,21 +138,22 @@ abstract contract Ax11Lp is IAx11Lp {
     function transferFrom(
         address from,
         address to,
+        uint256 poolId,
         uint256 longX,
         uint256 longY,
         uint256 shortX,
         uint256 shortY
     ) public returns (bool) {
         require(to != address(this), INVALID_ADDRESS());
-        require(allowance[from][msg.sender] >= block.timestamp, INSUFFICIENT_ALLOWANCE());
+        require(allowance[from][msg.sender][poolId] >= block.timestamp, INSUFFICIENT_ALLOWANCE());
 
-        LpInfo storage lpInfo_from = balanceOf[from];
+        LpInfo storage lpInfo_from = balanceOf[from][poolId];
         lpInfo_from.longX -= longX;
         lpInfo_from.longY -= longY;
         lpInfo_from.shortX -= shortX;
         lpInfo_from.shortY -= shortY;
 
-        LpInfo storage lpInfo_to = balanceOf[to];
+        LpInfo storage lpInfo_to = balanceOf[to][poolId];
         // Cannot overflow because the sum of all user
         // balances can't exceed the max uint256 value.
         unchecked {
@@ -145,7 +162,7 @@ abstract contract Ax11Lp is IAx11Lp {
             lpInfo_to.shortX += shortX;
             lpInfo_to.shortY += shortY;
         }
-        emit Transfer(from, to, longX, longY, shortX, shortY);
+        emit Transfer(from, to, poolId, longX, longY, shortX, shortY);
 
         return true;
     }
@@ -157,6 +174,7 @@ abstract contract Ax11Lp is IAx11Lp {
     /// @notice Approves a spender to transfer tokens using a signature
     /// @param owner The address of the token owner
     /// @param spender The address to approve or revoke permission for
+    /// @param poolId The poolId of the token
     /// @param value the block timestamp limit
     /// @param deadline The time at which the signature expires
     /// @param v The recovery byte of the signature
@@ -166,6 +184,7 @@ abstract contract Ax11Lp is IAx11Lp {
     function permit(
         address owner,
         address spender,
+        uint256 poolId,
         uint256 value,
         uint256 deadline,
         uint8 v,
@@ -184,12 +203,13 @@ abstract contract Ax11Lp is IAx11Lp {
                         keccak256(
                             abi.encode(
                                 keccak256(
-                                    'Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)'
+                                    'Permit(address owner,address spender,uint256 poolId,uint256 value,uint256 nonce,uint256 deadline)'
                                 ),
                                 owner,
                                 spender,
+                                poolId,
                                 value,
-                                nonces[owner]++,
+                                nonces[owner][poolId]++,
                                 deadline
                             )
                         )
@@ -201,10 +221,10 @@ abstract contract Ax11Lp is IAx11Lp {
             );
 
             require(recoveredAddress != address(0) && recoveredAddress == owner, INVALID_ADDRESS());
-            allowance[owner][spender] = value;
+            allowance[owner][spender][poolId] = value;
         }
 
-        emit Approval(owner, spender, value);
+        emit Approval(owner, spender, poolId, value);
         return true;
     }
 
@@ -221,7 +241,7 @@ abstract contract Ax11Lp is IAx11Lp {
             keccak256(
                 abi.encode(
                     0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f, //keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)")
-                    keccak256(bytes(name)),
+                    0xd9d99ffea2fbb94ab2706a9a675842d95e543e7e78c6bf9d1cf6fab7d873010d, //keccak256(bytes(name))
                     0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6, //keccak256("1")
                     block.chainid,
                     address(this)
@@ -235,18 +255,19 @@ abstract contract Ax11Lp is IAx11Lp {
 
     /// @notice Mints new LP tokens to an address
     /// @param to The address to mint tokens to
+    /// @param poolId The poolId of the token
     /// @param longX The amount of long position X tokens to mint
     /// @param longY The amount of long position Y tokens to mint
     /// @param shortX The amount of short position X tokens to mint
     /// @param shortY The amount of short position Y tokens to mint
-    function _mint(address to, uint256 longX, uint256 longY, uint256 shortX, uint256 shortY) internal {
-        LpInfo storage lpInfo_total = _totalSupply;
+    function _mint(address to, uint256 poolId, uint256 longX, uint256 longY, uint256 shortX, uint256 shortY) internal {
+        LpInfo storage lpInfo_total = _totalSupply[poolId];
         lpInfo_total.longX += longX;
         lpInfo_total.longY += longY;
         lpInfo_total.shortX += shortX;
         lpInfo_total.shortY += shortY;
 
-        LpInfo storage lpInfo_bal = balanceOf[to];
+        LpInfo storage lpInfo_bal = balanceOf[to][poolId];
         // Cannot overflow because the sum of all user
         // balances can't exceed the max uint256 value.
         unchecked {
@@ -256,23 +277,31 @@ abstract contract Ax11Lp is IAx11Lp {
             lpInfo_bal.shortY += shortY;
         }
 
-        emit Transfer(address(0), to, longX, longY, shortX, shortY);
+        emit Transfer(address(0), to, poolId, longX, longY, shortX, shortY);
     }
 
     /// @notice Burns LP tokens from an address
     /// @param from The address to burn tokens from
+    /// @param poolId The poolId of the token
     /// @param longX The amount of long position X tokens to burn
     /// @param longY The amount of long position Y tokens to burn
     /// @param shortX The amount of short position X tokens to burn
     /// @param shortY The amount of short position Y tokens to burn
-    function _burn(address from, uint256 longX, uint256 longY, uint256 shortX, uint256 shortY) internal {
-        LpInfo storage lpInfo_bal = balanceOf[from];
+    function _burn(
+        address from,
+        uint256 poolId,
+        uint256 longX,
+        uint256 longY,
+        uint256 shortX,
+        uint256 shortY
+    ) internal {
+        LpInfo storage lpInfo_bal = balanceOf[from][poolId];
         lpInfo_bal.longX -= longX;
         lpInfo_bal.longY -= longY;
         lpInfo_bal.shortX -= shortX;
         lpInfo_bal.shortY -= shortY;
 
-        LpInfo storage lpInfo_total = _totalSupply;
+        LpInfo storage lpInfo_total = _totalSupply[poolId];
         // Cannot underflow because a user's balance
         // will never be larger than the total supply.
         unchecked {
@@ -282,6 +311,6 @@ abstract contract Ax11Lp is IAx11Lp {
             lpInfo_total.shortY -= shortY;
         }
 
-        emit Transfer(from, address(0), longX, longY, shortX, shortY);
+        emit Transfer(from, address(0), poolId, longX, longY, shortX, shortY);
     }
 }
