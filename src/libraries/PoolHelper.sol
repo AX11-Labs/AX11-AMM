3d1e4e27819e74e03bab4388121ad3c9e1d6734c
Settee <ake14243@gmail.com>
2025-05-14 21:04:02 +0700

    add: a helper file for internal calculations

---
+++ src/libraries/PoolHelper.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.28;
+
+library PoolHelper {
+    error INVALID_BIN_ID();
+
+    function checkBinIdLimit(int24 value) private pure {
+        require(value >= -44405 && value <= 44405, INVALID_BIN_ID());
+    }
+
+    function computePoolId(address _tokenX, address _tokenY) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(_tokenX, _tokenY)));
+    }
+}
