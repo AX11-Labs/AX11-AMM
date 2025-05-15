727c1ccc969d195c2e9a8f66b4a5295a1c868bf7
Settee <ake14243@gmail.com>
2025-05-15 18:15:41 +0700

    update: change visibility to internal

--- src/libraries/PoolHelper.sol
+++ src/libraries/PoolHelper.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.28;
 library PoolHelper {
     error INVALID_BIN_ID();
 
-    function checkBinIdLimit(int24 value) private pure {
+    function checkBinIdLimit(int24 value) internal pure {
         require(value >= -44405 && value <= 44405, INVALID_BIN_ID());
     }
 
