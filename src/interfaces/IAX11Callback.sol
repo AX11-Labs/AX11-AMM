acc9242a1256a788d634a988afc2cefbf782119a
Settee <ake14243@gmail.com>
2025-05-15 18:16:54 +0700

    add: callbacks on mint and burn

---
+++ src/interfaces/IAX11Callback.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity 0.8.28;
+
+/// @title IAX11Callback
+/// @notice Any contract that calls any action in Pool.sol must implement this callback action
+interface IAX11Callback {
+    /// @notice Callback function interface for flashloan implementation
+    /// @param paybackAmount The amount of the token to be paid back, fee included.
+    function flashCallback(uint256 paybackAmount) external;
+
+    function mintCallback(uint256 payBackAmountX, uint256 payBackAmountY) external;
+
+    function burnCallback(
+        uint256 payBackLongXAmount,
+        uint256 payBackLongYAmount,
+        uint256 payBackShortXAmount,
+        uint256 payBackShortYAmount
+    ) external;
+}
